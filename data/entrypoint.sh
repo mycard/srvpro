ba709cfdbe2b48418090a6b4598028dd7be098f6
Nanahira <78877@qq.com>
2018-08-21 21:11:25 +0800

    fix

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -n "$authorized_keys" ] && [ ! -f /root/.ssh/authorized_keys ]; then printenv authorized_keys > /root/.ssh/authorized_keys; fi
+if [ -n "$authorized_keys" ] && [ ! -f /root/.ssh/authorized_keys ]; then mkdir /root/.ssh;  printenv authorized_keys > /root/.ssh/authorized_keys; chmod 600 /root/.ssh/authorized_keys; fi
 if [ -n "$password" ] && passwd --status | grep -q 'L'; then echo "root:$password" | chpasswd ; fi
 unset authorized_keys
 unset password
