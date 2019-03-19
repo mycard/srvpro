58ba490592407acd9d6d64b709819f1d91bd34b4
purerosefallen <78877@qq.com>
2019-03-19 22:24:08 +0800

    test new docker

--- data/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -n "$authorized_keys" ] && [ ! -f /root/.ssh/authorized_keys ]; then mkdir /root/.ssh;  printenv authorized_keys > /root/.ssh/authorized_keys; chmod 600 /root/.ssh/authorized_keys; fi
-if [ -n "$password" ] && passwd --status | grep -q 'L'; then echo "root:$password" | chpasswd ; fi
-unset authorized_keys
-unset password
-/usr/sbin/sshd -D
