b2ff4353b64d305b9297d00d09b2864cf67d21f1
purerosefallen <78877@qq.com>
2020-11-17 08:39:51 +0800

    fix reading version

--- ygopro-server.js
+++ ygopro-server.js
@@ -462,7 +462,7 @@
       }
     }
     try {
-      cppversion = parseInt((await fs.promises.readFile('ygopro/gframe/game.cpp', 'utf8').match(/PRO_VERSION = ([x\dABCDEF]+)/)[1]), '16');
+      cppversion = parseInt(((await fs.promises.readFile('ygopro/gframe/game.cpp', 'utf8'))).match(/PRO_VERSION = ([x\dABCDEF]+)/)[1], '16');
       await setting_change(settings, "version", cppversion);
       log.info("ygopro version 0x" + settings.version.toString(16), "(from source code)");
     } catch (error1) {
